ab16cd8bbb34ba1ed9bc18b9cbbd598aac2e76ee
Masty1988 <masty.dev@outlook.fr>
2026-01-22 15:44:14 +0100

    Update route.ts

--- src/app/api/adherents/[id]/route.ts
+++ src/app/api/adherents/[id]/route.ts
@@ -1,4 +1,5 @@
 // src/app/api/adherents/[id]/route.ts
+export const dynamic = 'force-dynamic';
 import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
